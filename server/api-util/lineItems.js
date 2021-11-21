a0b54f08422627065bef3e320158b3ad16388bcf
Tamas Bohus <71832383+tbohus@users.noreply.github.com>
2021-11-21 01:44:50 +0100

    upd

--- server/api-util/lineItems.js
+++ server/api-util/lineItems.js
@@ -4,8 +4,8 @@ const { Money } = types;
 
 // This bookingUnitType needs to be one of the following:
 // line-item/night, line-item/day or line-item/units
-const bookingUnitType = 'line-item/night';
-const PROVIDER_COMMISSION_PERCENTAGE = -10;
+const bookingUnitType = 'line-item/day';
+const PROVIDER_COMMISSION_PERCENTAGE = -30;
 
 /** Returns collection of lineItems (max 50)
  *
